d7aecf0fc1221306b499dcf38c4edabfe77f9843
Arturo Caliandro <73656521+artcal-polimi@users.noreply.github.com>
2021-03-22 17:09:29 +0100

    Added "model" folder and Color enumeration.

---
+++ src/main/java/it/polimi/ingsw/model/Color.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.model;
+
+/**
+ * This enumeration contains all the colors used by marbles and cards
+ */
+public enum Color {
+    WHITE,
+    RED,
+    GREEN,
+    YELLOW,
+    PURPLE,
+    BLUE,
+    GREY
+}
