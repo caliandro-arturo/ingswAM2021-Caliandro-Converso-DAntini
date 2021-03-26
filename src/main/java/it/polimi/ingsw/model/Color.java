61490b6f1a09691dbb8ffa3cceb98cb108cd3637
Arturo Caliandro <73656521+artcal-polimi@users.noreply.github.com>
2021-03-26 13:27:56 +0100

    Added market and update color attributes.

--- src/main/java/it/polimi/ingsw/model/Color.java
+++ src/main/java/it/polimi/ingsw/model/Color.java
@@ -4,11 +4,51 @@ package it.polimi.ingsw.model;
  * This enumeration contains all the colors used by marbles and cards
  */
 public enum Color {
-    WHITE,
-    RED,
-    GREEN,
-    YELLOW,
-    PURPLE,
-    BLUE,
-    GREY
+    BLUE {
+        @Override
+        void action(Player player) {
+            player.getBoard().addResource(Resource.SHIELD);
+        }
+    },
+    GREEN {
+        void action(Player player) {
+            /*only for cards*/
+        };
+    },
+    GREY {
+        @Override
+        void action(Player player) {
+            player.getBoard().addResource(Resource.STONE);
+        }
+    },
+    PURPLE {
+        @Override
+        void action(Player player) {
+            player.getBoard().addResource(Resource.SERF);
+        }
+    },
+    RED {
+        @Override
+        void action(Player player) {
+            FaithTrack path = player.getBoard().getPersonalPath();
+            path.setPosition(path.getPosition() + 1);
+        }
+    },
+    WHITE {
+        @Override
+        void action(Player player) {
+            /*checks player's leaderCards and choose between 3 options:
+                1. no leaderCard with WhiteMarbleConversion -> do nothing;
+                2. one leaderCard with WhiteMarbleConversion -> return resource;
+                3. two leaderCards //          //            -> ask the player which leader to use
+             */
+        }
+    },
+    YELLOW {
+        @Override
+        void action(Player player) {
+            player.getBoard().addResource(Resource.COIN);
+        }
+    };
+    abstract void action(Player player);
 }
