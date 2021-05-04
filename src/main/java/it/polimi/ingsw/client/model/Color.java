c8000715ed7e69971fadba5538dda6d2703c6b18
XenoVincent <71254499+XenoVincent@users.noreply.github.com>
2021-05-04 17:55:03 +0200

    modified color ascii art

--- src/main/java/it/polimi/ingsw/client/model/Color.java
+++ src/main/java/it/polimi/ingsw/client/model/Color.java
@@ -2,12 +2,12 @@ package it.polimi.ingsw.client.model;
 import it.polimi.ingsw.client.CLI.CLIColor;
 
 public enum Color {
-    GREEN(CLIColor.ANSI_GREEN.toString() + "[]" + CLIColor.RESET),
-    BLUE(CLIColor.ANSI_BLUE.toString() + "[]" + CLIColor.RESET),
-    YELLOW(CLIColor.ANSI_YELLOW.toString() + "[]" + CLIColor.RESET),
-    PURPLE(CLIColor.ANSI_PURPLE.toString() + "[]" + CLIColor.RESET),
-    RED(CLIColor.ANSI_RED.toString() + "[]" + CLIColor.RESET),
-    WHITE( "[]");
+    GREEN(CLIColor.ANSI_GREEN + "█" + CLIColor.RESET),
+    BLUE(CLIColor.ANSI_BLUE + "█" + CLIColor.RESET),
+    YELLOW(CLIColor.ANSI_YELLOW + "█" + CLIColor.RESET),
+    PURPLE(CLIColor.ANSI_PURPLE + "█" + CLIColor.RESET),
+    RED(CLIColor.ANSI_RED + "█" + CLIColor.RESET),
+    WHITE( "█");
     private final String representation;
 
     Color(String representation){
